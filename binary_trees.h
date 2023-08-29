8d7b8fc12d6b35d4997c187b6784e55831a872d9
Emad Anwer <emadanwer.official@gmail.com>
2023-08-29 15:03:08 +0300

    task 16

--- binary_trees.h
+++ binary_trees.h
@@ -45,7 +45,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-
+int binary_tree_is_perfect(const binary_tree_t *tree);
 /*-------End Functions-----------*/
 
 #endif /*BINARY_TREE_H*/
