18782952136030fc642b92ce77752e8f987e768c
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-29 12:37:29 +0300

    betty check

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 
 void traverse(const binary_tree_t *tree, size_t *counter);
+/**
+ * binary_tree_leaves - get the number of leaves of the tree
+ * @tree: the root of the tree
+ * Return: number of leaves of a tree
+ */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t no_leaves = 0;
@@ -10,6 +16,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	return (no_leaves);
 }
 
+/**
+ * traverse - traverse the tree and increment the counter if leave node
+ * @tree: the root of the tree
+ * @counter: the counter that stores the number of the leaves
+ */
 void traverse(const binary_tree_t *tree, size_t *counter)
 {
 	if (!tree)
