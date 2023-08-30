669115059b4144e5a13c21e0b9a5250025729ce0
Emad Anwer <emadanwer.official@gmail.com>
2023-08-30 19:06:16 +0300

    task 20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -23,7 +23,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 void binary_tree_levelorder_helper(const binary_tree_t *tree,
 								   void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
+	if (tree == NULL)
 		return;
 
 	if (tree->left)
