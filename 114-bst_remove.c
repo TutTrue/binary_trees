0df5497ce9778c21815c10a794f181a0bef6b713
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-31 19:58:37 +0300

    fix betty style

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,130 +1,63 @@
 #include "binary_trees.h"
 #include <stdio.h>
 bst_t *FindMin(bst_t *root);
-void swapNodes(bst_t *root, bst_t *min);
-
-/*bst_t *bst_remove(bst_t *root, int value)
+/**
+ * bst_remove - remove a node from a BST
+ * @root: root of the tree
+ * @value: value to be removed
+ * Return: root
+ */
+bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *rm_node, *min;
+	bst_t *min, *temp;
+
 	if (root == NULL)
 		return (root);
-	else if (value < root->n)
+
+	if (value < root->n)
 		root->left = bst_remove(root->left, value);
 	else if (value > root->n)
 		root->right = bst_remove(root->right, value);
 	else
 	{
-		if (root->left == NULL && root->right == NULL)
+		if (root->left == NULL)
 		{
+			temp = root->right;
+			if (root->parent != NULL)
+			{
+				if (root->parent->left == root)
+					root->parent->left = temp;
+				else
+					root->parent->right = temp;
+				if (temp != NULL)
+					temp->parent = root->parent;
+			}
 			free(root);
-			root = NULL;
-		}
-		else if (root->left == NULL)
-{
-    rm_node = root;
-    root = root->right;
-    if (rm_node->parent->left == rm_node)
-        rm_node->parent->left = root;
-    else
-        rm_node->parent->right = root;
-    if (root != NULL)
-        root->parent = rm_node->parent;
-    free(rm_node);
-}
-else if (root->right == NULL)
-{
-    rm_node = root;
-    root = root->left;
-    if (rm_node->parent->left == rm_node)
-        rm_node->parent->left = root;
-    else
-        rm_node->parent->right = root;
-    if (root != NULL)
-        root->parent = rm_node->parent;
-    free(rm_node);
-}*/
-		/*else if (root->left == NULL)
-		{
-			rm_node = root;
-			root = root->right;
-			root->parent = rm_node->parent;
-			root->parent->left = root;
-			free(rm_node);
+			return (temp);
 		}
 		else if (root->right == NULL)
 		{
-			rm_node = root;
-			root = root->left;
-			root->parent = rm_node->parent;
-			root->parent->right = root;
-			free(rm_node);
-		}*/
-		/*else
-		{
-
-			 
-			min = FindMin(root->right);
-
-			swapNodes(root, min);
-			root->parent->left = NULL;
+			temp = root->left;
+			if (root->parent != NULL)
+			{
+				if (root->parent->left == root)
+					root->parent->left = temp;
+				else
+					root->parent->right = temp;
+				if (temp != NULL)
+					temp->parent = root->parent;
+			}
 			free(root);
-			root = min;
-
+			return (temp);
+		}
+		else
+		{
+			min = FindMin(root->right);
+			root->n = min->n;
+			root->right = bst_remove(root->right, min->n);
 		}
 	}
 	return (root);
-}*/
-bst_t *bst_remove(bst_t *root, int value)
-{
-	bst_t *min, *temp;
-    if (root == NULL)
-        return root;
-
-    if (value < root->n)
-        root->left = bst_remove(root->left, value);
-    else if (value > root->n)
-        root->right = bst_remove(root->right, value);
-    else
-    {
-        if (root->left == NULL)
-        {
-            temp = root->right;
-            if (root->parent != NULL)
-            {
-                if (root->parent->left == root)
-                    root->parent->left = temp;
-                else
-                    root->parent->right = temp;
-                if (temp != NULL)
-                    temp->parent = root->parent;
-            }
-            free(root);
-            return temp;
-        }
-        else if (root->right == NULL)
-        {
-            temp = root->left;
-            if (root->parent != NULL)
-            {
-                if (root->parent->left == root)
-                    root->parent->left = temp;
-                else
-                    root->parent->right = temp;
-                if (temp != NULL)
-                    temp->parent = root->parent;
-            }
-            free(root);
-            return temp;
-        }
-        else
-        {
-            min = FindMin(root->right);
-            root->n = min->n;
-            root->right = bst_remove(root->right, min->n);
-        }
-    }
-
-    return root;
 }
 /**
  * FindMin - find minimum in a tree.
@@ -139,7 +72,7 @@ bst_t *FindMin(bst_t *root)
 	return root;
 }
 
-void swapNodes(bst_t *root, bst_t *min)
+/*void swapNodes(bst_t *root, bst_t *min)
 {
 	bst_t *tempParent, *tempLeft, *tempRight;
 
@@ -174,7 +107,7 @@ void swapNodes(bst_t *root, bst_t *min)
 	min->right->parent = min;
 	min->left->parent = min;
 }
-/*void swapNodesn(bst_t *root, bst_t *min)
+void swapNodesn(bst_t *root, bst_t *min)
 {
     int temp = root->n;
 
