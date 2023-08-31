2d8fa3496ecfe2a2c98cafef6ced27d0f62ba95e
Emad Anwer <emadanwer.official@gmail.com>
2023-08-31 19:33:52 +0300

    task 20 fix

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
-void binary_tree_levelorder_helper(const binary_tree_t *tree,
-								   void (*func)(int));
+/*functions protypes*/
+void enqueue(Queue_t *queue, binary_tree_t *treeNode);
+binary_tree_t *dequeue(Queue_t *queue);
+void freeQueue(Queue_t *queue);
 /**
  * binary_tree_levelorder - traverse the tree in post order
  * @tree: the root of the tree
@@ -9,32 +11,96 @@ void binary_tree_levelorder_helper(const binary_tree_t *tree,
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	int *x;
+	Queue_t *queue = NULL;
+	binary_tree_t *current;
+
 	if (tree == NULL || func == NULL)
 		return;
+	queue = (Queue_t *)malloc(sizeof(Queue_t));
+	queue->front = queue->rear = NULL;
 
-	x = malloc(sizeof(int));
-	free(x);
-	func(tree->n);
-	binary_tree_levelorder_helper(tree, func);
+	enqueue(queue, (binary_tree_t *)tree);
+
+	while (queue->front != NULL)
+	{
+		current = (binary_tree_t *)queue->front->treeNode;
+		dequeue(queue);
+
+		if (current)
+		{
+			func(current->n);
+			enqueue(queue, current->left);
+			enqueue(queue, current->right);
+		}
+	}
+	freeQueue(queue);
 }
-/**
- * binary_tree_levelorder_helper - traverse the tree in post order
- * @tree: the root of the tree
- * @func: a func that will be fired on the data of each node
- */
-void binary_tree_levelorder_helper(const binary_tree_t *tree,
-								   void (*func)(int))
-{
-	if (tree == NULL)
-		return;
 
-	if (tree->left)
-		func(tree->left->n);
-	if (tree->right)
-		func(tree->right->n);
-	if (tree->left)
-		binary_tree_levelorder_helper(tree->left, func);
-	if (tree->right)
-		binary_tree_levelorder_helper(tree->right, func);
+/**
+ * enqueue - add new node the end of queue
+ * @queue: pointer to queue
+ * @treeNode: integer value
+ */
+void enqueue(Queue_t *queue, binary_tree_t *treeNode)
+{
+	struct QueueNode *newNode;
+
+	newNode = (struct QueueNode *)malloc(sizeof(struct QueueNode));
+	if (newNode == NULL)
+		return;
+	newNode->treeNode = treeNode;
+	newNode->next = NULL;
+
+	if (queue->rear == NULL)
+	{
+		queue->front = queue->rear = newNode;
+	}
+	else
+	{
+		queue->rear->next = newNode;
+		queue->rear = newNode;
+	}
+}
+
+/**
+ * dequeue - delete queue node from top
+ * @queue: pointer to queue
+ * Return: node pointer
+ */
+binary_tree_t *dequeue(Queue_t *queue)
+{
+	struct QueueNode *tempNode;
+	binary_tree_t *treeNode;
+
+	if (queue->front == NULL)
+		return (NULL);
+
+	tempNode = queue->front;
+	treeNode = tempNode->treeNode;
+
+	queue->front = queue->front->next;
+	if (queue->front == NULL)
+	{
+		queue->rear = NULL;
+	}
+
+	free(tempNode);
+	return (treeNode);
+}
+
+/**
+ * freeQueue - free queue by call dequeue
+ * @queue: pointer to queue
+ */
+void freeQueue(Queue_t *queue)
+{
+	struct QueueNode *tempNode;
+
+	while (queue->front != NULL)
+	{
+		tempNode = queue->front;
+		queue->front = queue->front->next;
+		free(tempNode);
+	}
+	free(queue);
 }
